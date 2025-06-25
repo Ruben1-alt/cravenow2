2c7111e967b683560d3b2e197f2e6ec56d965fc7
Ruben1-alt <ruben01587@gmail.com>
2025-06-25 16:25:41 +0530

    tir commit

--- src/pages/Guest/Homepage.jsx
+++ src/pages/Guest/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
-import Footer from '../../components/footer'
+// import Footer from '../../components/footer'
 import Content from '../../components/Content'
 import Featured from '../../components/Featured'
 
